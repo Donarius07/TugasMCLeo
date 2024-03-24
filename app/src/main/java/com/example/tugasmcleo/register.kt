a9ee7d0106c59905164149bd9ba872d222d18bdb
TUGAS3MC <alopkalakmabin@gmail.com>
2024-03-24 23:03:09 +0900

    Initial commit

---
+++ app/src/main/java/com/example/tugasmcleo/register.kt
@@ -0,0 +1,11 @@
+package com.example.tugasmcleo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
